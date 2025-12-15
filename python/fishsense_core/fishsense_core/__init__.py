2ae05147b42f947c5505eb15b278677c9a6a5e6a
Christopher L. Crutchfield <ccrutchf@ucsd.edu>
2025-12-14 22:50:55 -0800

    fix: export the native module

--- python/fishsense_core/fishsense_core/__init__.py
+++ python/fishsense_core/fishsense_core/__init__.py
@@ -1,3 +1,6 @@
 """FishSense Core Package"""
 
+from . import _native as _native
+laser = _native.laser
+
 __version__ = "0.1.0"
--- python/fishsense_core/fishsense_core/laser.py
+++
@@ -1,3 +0,0 @@
-import fishsense_core
-
-calibrate_laser = fishsense_core._native.laser.calibrate_laser
