71f5c8743a96caad9b761c005dd698b59fa260bd
Christopher L. Crutchfield <ccrutchf@ucsd.edu>
2026-04-08 21:54:48 -0700

    fix: pylint errors

--- python/fishsense_core/fishsense_core/laser.py
+++ python/fishsense_core/fishsense_core/laser.py
@@ -1,3 +1,4 @@
+"""Functions for controlling the laser."""
 from fishsense_core import _native
 
-calibrate_laser = _native.laser.calibrate_laser
+calibrate_laser = _native.laser.calibrate_laser # pylint: disable=c-extension-no-member
