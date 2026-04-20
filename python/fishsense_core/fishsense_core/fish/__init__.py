a60bd33d4841c49ea5949a12b5cfbb341b37bb48
Christopher L. Crutchfield <ccrutchf@ucsd.edu>
2026-04-20 07:52:45 -0700

    fix: pylint errors

--- python/fishsense_core/fishsense_core/fish/__init__.py
+++ python/fishsense_core/fishsense_core/fish/__init__.py
@@ -1,3 +1,5 @@
+"""Fish detection, segmentation, and measurement."""
+# pylint: disable=c-extension-no-member,no-name-in-module,import-error
 from fishsense_core._native.fish import FishSegmentation
 
 __all__ = ["FishSegmentation"]
